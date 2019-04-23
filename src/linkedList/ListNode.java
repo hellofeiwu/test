48da80f3e876292f8e7e7c49beb5bcdd7cbce29c
Wu <FWU31@ford.com>
2019-04-23 14:40:54 +0800

    add demo solution

--- src/linkedList/ListNode.java
+++ src/linkedList/ListNode.java
@@ -99,6 +99,21 @@ public class ListNode {
         }
     }
 
+    static class Solution5 {
+        public ListNode removeElements(ListNode head, int val) {
+            if(head == null) {
+                return head;
+            }
+            ListNode res = removeElements(head.next, val);
+            if(head.val == val) {
+                return res;
+            }else {
+                head.next = res;
+                return head;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {7,6,6,6,3,4,5,6};
         ListNode head = new ListNode(arr[0]);
@@ -109,7 +124,7 @@ public class ListNode {
         }
 
         System.out.println(head);
-        System.out.println((new Solution4()).removeElements(head, 6));
+        System.out.println((new Solution5()).removeElements(head, 6));
     }
 }
 
